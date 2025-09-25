58dce01db46930c71bb1d4c675a73ce1616e5f01
Oleg Shapoval <olegkazhan@gmail.com>
2025-09-25 15:12:42 +0300

    finish banner & header

--- src/components/Banner/Banner.jsx
+++ src/components/Banner/Banner.jsx
@@ -1,13 +1,18 @@
 import css from "./Banner.module.css";
 import bannerImg from "../../../public/Banner.webp";
+import { Link } from "react-router-dom";
 export default function Banner() {
   return (
     <div className={css.banner}>
       <img src={bannerImg} alt="Banner" className={css.bannerImg} />
       <div className={css.textBannerContainer}>
-        <h1 className={css.secondTitle}>Find your perfect rental car</h1>
-        <h2>Reliable and budget-friendly rentals for any journey</h2>
-        <button className={css.btnBanner}>View Catalog</button>
+        <h1 className={css.firstBannerTitle}>Find your perfect rental car</h1>
+        <h2 className={css.secondBannerTitle}>
+          Reliable and budget-friendly rentals for any journey
+        </h2>
+        <Link to="/catalog">
+          <button className={css.btnBanner}>View Catalog</button>
+        </Link>
       </div>
     </div>
   );
