cf1e8abe7bbc45747183ff4a44708ce83c1d523a
Oleg Shapoval <olegkazhan@gmail.com>
2025-09-27 18:30:25 +0300

    meta for main

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,8 +1,16 @@
 import Banner from "../Banner/Banner.jsx";
+import { Helmet } from "react-helmet";
 
 export default function Home() {
   return (
     <>
+      <Helmet>
+        <title>Rental Car - Find your perfect rental car</title>
+        <meta
+          name="description"
+          content={`Reliable and budget-friendly rentals for any journey`}
+        />
+      </Helmet>
       <Banner />
     </>
   );
